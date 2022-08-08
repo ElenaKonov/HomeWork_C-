564c410fb26ab303012eeb4881f23c5844b17395
ElenaKonov <nega-le@yandex.ru>
2022-08-09 00:59:51 +0500

    add Task_3 % even/odd of number

---
+++ Task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите  число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if( numberA%2 == 0)
+{
+    System.Console.WriteLine("Число является четным.");
+}
+else
+{
+    System.Console.WriteLine("Число является нечетным");
+}
+   
+
+   
+
