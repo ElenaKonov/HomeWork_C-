8234b4c16fd46573b0226145cb3bd4d16a279cbb
ElenaKonov <nega-le@yandex.ru>
2022-08-09 00:18:45 +0500

    add Task_1 - max from numbers

---
+++ Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите первое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber == secondNumber)
+{
+    System.Console.WriteLine( "ВВедены одинаковые числа");
+}
+else
+
+       if (firstNumber > secondNumber)
+{
+    System.Console.WriteLine ("Максимальным является число " + firstNumber);
+}
+
+if (secondNumber > firstNumber)
+{
+    System.Console.WriteLine ("Максимальным является число " + secondNumber);
+}
+
+
