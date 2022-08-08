f8f563804199ba8f4f01bbeae6691ea1e1f170fc
ElenaKonov <nega-le@yandex.ru>
2022-08-09 01:45:47 +0500

    add Task_4: sequence of even numbers

---
+++ Task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите  число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+ 
+ int counter = 1;
+
+ while(counter <= numberA)
+ {
+    if (counter % 2 == 0) 
+    {
+        System.Console.WriteLine(counter);
+        
+    }
+    counter++;
+ }
