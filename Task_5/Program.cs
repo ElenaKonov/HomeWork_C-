5121a774444a29fd6c2c81189903581297d80c87
ElenaKonov <nega-le@yandex.ru>
2022-08-10 15:06:56 +0500

    add programm about the second digit

---
+++ Task_5/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Нужно сделать через числа, без индексов строк.
+456 -> 5
+782 -> 8
+918 -> 1
+*/ 
+Console.WriteLine("Введите  число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if(numberA > 99 && numberA < 1000)
+{
+int secondDigit = numberA % 100 / 10;
+
+
+Console.WriteLine($"Вторая   цифра числа {numberA}: {secondDigit}");
+}
+
+else
+{
+    Console.WriteLine("Введенное число не подходит по условию задачи.");
+}
