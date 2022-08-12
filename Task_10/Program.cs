6c9dbad61e6e361a00c3d6887006a4ccc76067a6
ElenaKonov <nega-le@yandex.ru>
2022-08-12 16:54:13 +0500

    Task about number cubes

---
+++ Task_10/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 23
+
+Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+ 
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int start = 1; start <= n; start++)
+{
+Console.Write(Math.Pow(start,3)+"\t");
+}
