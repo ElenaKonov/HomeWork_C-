913909e6e64aff0fa4270272a38d76b06df0d500
ElenaKonov <nega-le@yandex.ru>
2022-08-20 01:27:16 +0500

    Add program about sum  of  numbers in array with odd  position

---
+++ Task_16/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и 
+минимальным элементами массива.
+
+[3 7 22 2 78] -> 76
+*/
+double[] GetArray(int size)
+{
+double[] result = new  double[size];
+for (int i=0; i< size; i++){
+    result[i]= Math.Round((new Random().NextDouble())*100);
+}
+  return  result; 
+  }
+  System.Console.WriteLine("Введите размер массива");
+
+  int size=Convert.ToInt32(Console.ReadLine());
+  double[] array=GetArray(size);
+
+double max =array[0]; double min=array[0];
+for (int i=1; i< size; i++){
+
+   if (array[i]> max ) max=array[i];
+ else min =array[i];
+}
+
+double difference=max-min;
+
+
+System.Console.WriteLine($" Разность между максимальным  {max}  и минимальным  {min} элементами массива [{String.Join(" ; " , array)}] равна {difference}");
