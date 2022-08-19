e7ec7dbc4876dd08277ee49b04db14a50451cf8e
ElenaKonov <nega-le@yandex.ru>
2022-08-20 00:28:32 +0500

    Add program about counting of even number in array

---
+++ Task_14/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+*/
+
+
+int[] GetArray(int size)
+{
+int[] result = new int [size];
+for (int i=0; i< size; i++){
+    result[i]= new Random().Next(100,1000);
+}
+  return  result; 
+  }
+  System.Console.WriteLine("Введите размер массива");
+
+  int size=Convert.ToInt32(Console.ReadLine());
+  int[] array=GetArray(size);
+
+int GetCountEven(int[] array)
+{
+int count=0; int i=0;
+while (i < size ){
+    if(array[i]%2 == 0)count++;
+    i++;
+}
+return count;
+}
+int counter= GetCountEven(array);
+System.Console.WriteLine($" Количество четных чисел в массиве[{String.Join("," , array)}] равно {counter}");
