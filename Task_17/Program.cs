8f3ddbe70e3d55a8a297c65589a024147593343f
ElenaKonov <nega-le@yandex.ru>
2022-08-24 16:18:01 +0500

    add programm about count  positive numbers

---
+++ Task_17/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+
+//1, -7, 567, 89, 223-> 3
+
+
+int[] GetArray(int size)
+{
+int[] result = new int [size];
+for (int i=0; i< size; i++){
+    Console.WriteLine("Введите  число");
+     result[i]=Convert.ToInt32(Console.ReadLine());
+}
+  return  result; 
+  }
+  Console.WriteLine("Введите колличество чисел");
+
+  int size=Convert.ToInt32(Console.ReadLine());
+  int[] array=GetArray(size);
+
+int GetCountPositive(int[] array)
+{int count=0;
+    for( int i=0; i<size; i++){
+
+       if(array[i]>0)count++;
+
+}
+return count;
+}
+int counter= GetCountPositive(array);
+Console.WriteLine($" Количество положительныъ чисел  среди введенных [{String.Join("," , array)}] равно {counter}");
