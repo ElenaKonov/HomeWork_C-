57031df2ec140942d5dd9a615d138775c0589024
ElenaKonov <nega-le@yandex.ru>
2022-08-25 04:14:19 +0500

    add task about cod

---
+++ Task_19_1/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 42: Напишите программу, которая будет 
+//преобразовывать десятичное число в двоичное.
+//45 -> 101101
+//3 -> 11
+//2 -> 10
+
+
+
+
+//int counter=GetLengthBinaryCod(number);
+Console.WriteLine("Введите число");
+
+int number=Convert.ToInt32(Console.ReadLine());
+int size=Convert.ToInt32(Math.Round(Math.Log2(number))+1);
+
+
+int[] GetBinaryCod( int size){
+int[] result=new int[ size];
+
+while (number>0){
+     for (int i=0; i< size; i++) {
+     result[size-1-i] =number%2;
+ number=number/=2;
+}
+}
+return result;
+}
+int [] array=GetBinaryCod(size);
+
+
+
+Console.WriteLine($"[{String.Join("", array)}]");
+
