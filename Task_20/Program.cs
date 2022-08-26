a74a8c24274e6712c50c1f0af4d63097625edb87
ElenaKonov <nega-le@yandex.ru>
2022-08-26 14:19:38 +0500

    add task double matrix

--- Task_20/Program.cs
+++ Task_20/Program.cs
@@ -22,7 +22,7 @@ for (int i = 0; i < matrix.GetLength(0); i++)
 
 for (int j = 0; j < matrix.GetLength(1); j++) 
 {
-matrix[i, j] =  Math.Round(new Random().NextDouble()*100,2);
+matrix[i, j] = -1* Math.Round((new Random().NextDouble()*2-1)*10,2);
 }
 }
 return matrix;
