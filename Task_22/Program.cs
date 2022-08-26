73ba3fd76d09c817f521dd477b4b0945b5316237
ElenaKonov <nega-le@yandex.ru>
2022-08-26 20:21:03 +0500

    add task about sum elements

---
+++ Task_22/Program.cs
@@ -0,0 +1,47 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int size=columns;
+
+int[,] matrix = new int[rows, columns];
+
+for (int i = 0; i < matrix.GetLength(0); i++) {
+        for (int j = 0; j < matrix.GetLength(1); j++) {
+              matrix[i, j] = new Random().Next(-10,10); 
+
+               Console.Write(matrix[i, j] + "\t");
+               }
+         Console.WriteLine();
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Сумма элементов столбцов матрицы:");
+
+Console.WriteLine();
+
+
+int[]sum =new int [size];
+
+for (int j = 0; j < size; j++) {
+
+ 
+    for (int i= 0; i < matrix.GetLength(0); i++) {
+        
+sum [j]+=matrix[i,j]; 
+    }
+Console.Write(sum[ j] + "\t");
+}
+
+   
+
+ 
