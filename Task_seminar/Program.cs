f6be5776968f21c7587ed7e6acade603233671d2
ElenaKonov <nega-le@yandex.ru>
2022-08-26 13:45:58 +0500

    add changes

---
+++ Task_seminar/Program.cs
@@ -0,0 +1,39 @@
+﻿int[] array = GetArray(10, 0, 10);
+Console.WriteLine(String.Join(" ", array));
+
+int[] reversArray=CopyArray(array);
+Console.WriteLine(String.Join(" ", reversArray));
+
+ReversArray(array);
+Console.WriteLine(String.Join(" ", array));
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+int[] res = new int[size];
+for (int i = 0; i < size; i++) // size = array.Length
+{
+res[i] = new Random().Next(minValue, maxValue + 1); // [0,5) => [0;4]
+}
+return res;
+}
+
+void ReversArray(int[] inArray)
+{
+for (int i = 0; i < inArray.Length / 2; i++)
+{
+int temp = inArray[i];
+inArray[i] = inArray[inArray.Length - i - 1];
+inArray[inArray.Length - i - 1] = temp;
+}
+}
+
+int[] CopyArray(int[] inArray)
+{
+int[] result = new int[inArray.Length]; // size = inArray.Length
+for (int i = 0; i < inArray.Length; i++)
+{
+result[i] = inArray[inArray.Length - 1 - i];
+
+}
+return result;
+}
