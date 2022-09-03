091e77e3ca9cbd6e832f97185f43aa4b1b189dfd
ElenaKonov <nega-le@yandex.ru>
2022-09-03 14:43:15 +0500

    add changes

--- Task_24/Program.cs
+++ Task_24/Program.cs
@@ -15,59 +15,52 @@ int rows = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество столбцов: ");
 int columns = Convert.ToInt32(Console.ReadLine());
 
+int [,]GetArray(int rows, int colums){
 
-int [,] GetArray(int m, int n)
-{
-int  [,] matrix = new int[m, n];
-for (int i = 0; i < matrix.GetLength(0); i++) 
-{
+               int  [,] matrix = new int[rows, columns];
+                   for (int i = 0; i < matrix.GetLength(0); i++) {
 
-for (int j = 0; j < matrix.GetLength(1); j++) 
-{
-matrix[i, j] = new Random().Next(-10,10);
-}
-}
-return matrix;
+                           for (int j = 0; j < matrix.GetLength(1); j++) {
+                                matrix[i, j] = new Random().Next(-10,10);
+                            }
+                    }
+                return matrix;
 }
 
 
-void PrintArray(int[,] inputMatrix)
-{
-for (int i = 0; i <inputMatrix.GetLength(0); i++)
-{
-for (int j = 0; j < inputMatrix.GetLength(1); j++)
-{
-Console.Write(inputMatrix[i, j] + "\t"); 
+void PrintArray(int[,] inputMatrix){
+       
+             for (int i = 0; i <inputMatrix.GetLength(0); i++) {
+                 for (int j = 0; j < inputMatrix.GetLength(1); j++){
+                      Console.Write(inputMatrix[i, j] + "\t"); 
+                  }
+               Console.WriteLine();
+                }
 }
-Console.WriteLine();
-}
-}
+
 int[,] resultMatrix = GetArray(rows, columns);
 PrintArray(resultMatrix);
 
 
 int [] GetArraySum(int[,] resultMatrix){
-int[]sum =new int [rows];
+       int[]sum =new int [rows];
 
-for (int i = 0; i < resultMatrix.GetLength(0); i++) {
+        for (int i = 0; i < resultMatrix.GetLength(0); i++) {
 
-        for (int j= 0; j < resultMatrix.GetLength(1); j++) {
+          for (int j= 0; j < resultMatrix.GetLength(1); j++) {
         
              sum [i]+=resultMatrix[i,j]; 
           }
-     } 
-     return sum;
+          } 
+         return sum;
 }  
 
 
-void PrintArrayNew(int[] inputMatrix)
-{
-for (int i = 0; i <inputMatrix.Length; i++)
-
-{
-Console.WriteLine(inputMatrix[i] + "\t"); 
-}
-Console.WriteLine();
+void PrintArrayNew(int[] inputMatrix){
+        for (int i = 0; i <inputMatrix.Length; i++){
+          Console.WriteLine(inputMatrix[i] + "\t"); 
+         }
+        Console.WriteLine();
 }
 
 int[] resultSum = GetArraySum(resultMatrix);
@@ -78,15 +71,13 @@ PrintArrayNew(resultSum);
 
 
 int  GetRowMinSum(int[] sum){
-
-
-int min=sum[0]; int index=0;
-for (int i=1; i< sum.Length; i++){
-  if (sum[i]< min ) min=sum[i];
-  if (min==sum[i]){index=i;}
+      int min=sum[0]; int index=0;
+         for (int i=1; i< sum.Length; i++){
+            if (sum[i]< min ) min=sum[i];
+                if (min==sum[i])index=i;
    
  
-}
+          }
 return index;
 }
 
