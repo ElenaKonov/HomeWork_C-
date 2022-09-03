a6f36614918f0bf9988626ad2efd9e7386037d75
ElenaKonov <nega-le@yandex.ru>
2022-09-03 19:27:58 +0500

    add ефыл фищге ыгь

---
+++ Task_29/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных 
+//элементов в промежутке от M до N.
+
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+Console.Write("Введите  первое число : ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите  второечисло: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+//int sum=0;
+/// start  = начало (M), end - конец (N)
+int SumNumbers(int start, int end)
+{
+    // Базовый случай
+    if (start == end) return M; // start == end (start == N), возращаем число
+    // Рекурсивный случай
+    return (end+SumNumbers(start,end-1));
+}
+
+Console.WriteLine($"Сумма чисел от {M} до {N}: {SumNumbers(M, N)}");
+
